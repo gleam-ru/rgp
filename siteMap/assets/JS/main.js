a14896a6ca7cca1c6bfee5d51ab3bae6d7f1c6ab
gleam-ru <roman.demeshenko@gmail.com>
2014-02-26 11:17:17 +0400

    getPath_02

--- siteMap/assets/JS/main.js
+++ siteMap/assets/JS/main.js
@@ -72,80 +72,8 @@ window.tree = {
 	}
 }
 
-window.q = {
 
-}
-
-var BCCreator = function() {
-	this.tree = {
-		classes: {
-			name: "Информационная модель",
-			editAttribute: {
-				name: "Информация об атрибуте",
-
-			}
-		},
-		items: {
-			name: "Список объектов класса %className%",
-			editItem: {
-				name: "Редактирование экземпляра #%itemId%",
-				attributeValues: {
-					//TODO: что это? зачем нужно? почему доступ возможен и через меню?
-					name: "Отчет"
-				}
-			}
-		},
-		roadMaps: {
-			name: "Маршрутные карты",
-			roadMap: {
-				name: "Маршрутная карта #%roadMapId%",
-				step: {
-					name: "Этап #%stepId%",
-					stepItems: {
-						name: "Осматриваемое оборудование и точки",
-						stepItemAttribute: {
-							//TODO: как сюда попасть?
-							name: "",
-						}
-					}
-				}
-			}
-		},
-		barcodes: {
-			name: "Метки",
-			barcode: {
-				name: "Метка #%barcodeId%",
-			}
-		},
-		rounds: {
-			name: "Обходы",
-			round: {
-				name: "Обход #%roundId%"
-			}
-		},
-		//TODO: тут должны быть "отчеты", но зачем они нужны - пока неизвестно...
-		comments: {
-			name: "Комментарии для класса: %className%",
-		},
-		users: {
-			name: "Пользователи",
-			user: {
-				name: "Пользователь: %userName%",
-			}
-		},
-		settings: {
-			name: "Настройки",
-
-		},
-		login: {
-			name: "Авторизация",
-
-		},
-		index: {
-			name: "Добро пожаловать!",
-		}
-	}
-}
+var BCCreator = function() {}
 
 function getNames(tree) {
 	if(tree) {
@@ -166,13 +94,17 @@ function getNames(tree) {
 function getPath(name, tree) {
 	if(tree) {
 		var namesList = [];
-		if(tree.name == name) {
-			namesList.push(tree.name);
-			for (var key in tree) if(tree.hasOwnProperty(key)) {
-				namesList = namesList.concat(getPath(name, tree[key]));
+		if(tree.name) {
+			if(tree.name == name) {
+				return namesList.push(name);
 			}
+			else {
+				for(var key in tree) if(tree.hasOwnProperty(key)) {
+					return getPath(name, tree[key]);
+				}
+			}
+			return namesList;
 		}
-		return namesList;
 	}
 	else {
 		return undefined;
